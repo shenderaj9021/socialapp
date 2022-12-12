ece04667252cd6bd07407ae7ae762f7196e731e7
Raj <rajshende220802@gmail.com>
2022-12-13 02:04:29 +0530

    cors added

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "https://socialappclient.vercel.app"
+
 };
 
 app.use(cors(corsOptions));
